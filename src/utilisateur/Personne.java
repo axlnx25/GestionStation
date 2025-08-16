addbdeab26ca12cfb21ac6a3d317c18754d3f302
axlnx25 <ouedraogoabdunnourr@gmail.com>
2025-08-16 08:43:54 +0000

    modulation du projet

---
+++ src/utilisateur/Personne.java
@@ -0,0 +1,9 @@
+package utilisateur;
+
+public class Personne {
+    private int identifiant;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String telephone;
+}
